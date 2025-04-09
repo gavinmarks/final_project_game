6c428f893b15c27a903da8b582b8ecddc56a06c4
IndOStrialist <brandmh20@gmail.com>
2025-04-09 14:09:13 -0500

    Citylization_v0.1.3
    -made it so grid borders are (somewhat) scaled to the gridSize. *Not perfect but can render up to at least 50 by 50 squares before their borders cause them to be invisble.

---
+++ Citylization_v0.1.3.py
@@ -0,0 +1,88 @@
+import pygame
+import math
+
+pygame.init()
+
+gridSize = 40
+width, height = 600, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("CivLike v0.1.2")
+
+def scaleAdjust(tiles):
+    print("width: ", width, "| height: ", height)
+    returnScale = min(width, height)/tiles
+    print("the scale is:", returnScale)
+    return returnScale
+
+scale = scaleAdjust(gridSize)
+
+
+#you got me fucked up if you think im putting all those coordinates in a list manually
+tile_cords = []
+tile_x_cords = []
+tile_y_cords = []
+count = 0
+
+for i in range(gridSize):
+    tile_x_cords.append(count)
+    tile_y_cords.append(count)
+    count = count + 1
+
+for i1 in tile_x_cords:
+    for i2 in tile_y_cords:
+        tile_cords.append((i1, i2))
+
+
+class Tile:
+    def __init__(self, x, y, size, color,):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.color = color
+        self.selected = False
+        self.rect = pygame.Rect(x, y, size, size)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+        if self.selected:
+            pygame.draw.rect(screen, pygame.Color("white"), self.rect, round((2*self.size)/scale))
+        else:
+            pygame.draw.rect(screen, pygame.Color("black"), self.rect, round((2*self.size)/scale))
+
+    def handle_click(self, pos):
+        if self.rect.collidepoint(pos):
+            self.selected = not self.selected
+            return True
+        return False
+
+
+more_tiles = []
+for x, y in tile_cords:
+    tiles = Tile(x*scale, y*scale, scale, (pygame.Color("green")))
+    more_tiles.append(tiles)
+
+
+running = True
+
+while running:
+    screen.fill(pygame.Color("white")) 
+
+
+    for tile in more_tiles:
+        tile.draw(screen)
+
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            for tile in more_tiles:
+                tile.selected = False
+            for tile in more_tiles:
+                if tile.handle_click(pos):
+                    print(f"Tile at ({tile.x}, {tile.y}) was clicked")
+                    break
+        elif event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+
+pygame.quit()
