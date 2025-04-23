ba0210eb0ff2543f76c80c1f41cf9b5cf8999916
IndOStrialist <brandmh20@gmail.com>
2025-04-23 10:50:55 -0500

    GUI Module 0.0.1

--- Citylization_GUI_Module.py
+++ Citylization_GUI_Module.py
@@ -6,8 +6,11 @@ scale = 100
 width, height = 600, 600
 screen = pygame.display.set_mode((width, height))
 
+renderListP = []
+renderListS = []
+
 class button:
-    def __init__(self, x, y, SizeX, SizeY, text):
+    def __init__(self, x, y, SizeX, SizeY, Frame, text):
         self.x = x
         self.y = y
         self.SizeX = SizeX
@@ -16,10 +19,13 @@ class button:
         self.scale = scale
         self.text = text
         self.rect = pygame.Rect(self.x, self.y, self.SizeX, self.scale)
+        self.frame = Frame
 
-    def draw(self, frame):
-        pygame.draw.rect(screen, "red", self.rect)
-        print(self.x, self.y, self.scale)
+    def draw(self, screen):
+        pygame.draw.rect(screen, "black", self.rect)
+        
+    def close(self):
+        del self
 
 class clickMenu:
     def __init__(self, x, y, scale, options):
@@ -28,13 +34,27 @@ class clickMenu:
         self.name = 'popupMenu'
         self.scale = scale
         self.rect = pygame.Rect(self.x, self.y, self.scale*2, self.scale*4)
+        self.options = options
+        self.buttons = []
+        for option in options:
+            adjustY = self.y + scale
+            buttonkey = button(self.x, self.y + adjustY, self.scale*1.5, self.scale, self.rect, option)
+            buttonkey.draw(screen)
+            print(buttonkey)
+            renderListS.append(buttonkey)
+            self.buttons.append(buttonkey)
+            
+        
         
     def close(self):
+        for buttonkey in self.buttons:
+            buttonkey.close
+            renderList.remove(buttonkey)
+        print("delete check")
         del self
         
     def draw(self, screen):
         pygame.draw.rect(screen, "white", self.rect)
-        print(self.x, self.y, self.scale)
 
     
 
@@ -45,22 +65,34 @@ class clickMenu:
 popMenu = 0
 running = True
 while running:
-    print(popMenu)
     screen.fill("black")
-    if popMenu != 0:
-        popMenu.draw(screen)
+    for item in renderListP:
+        item.draw(screen)
+        
+    for item in renderListS:
+        item.draw(screen)
 
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 3:
                 if popMenu != 0:
-                    popMenu.close()
+                    print("popmenu", popMenu)
+                    popMenu.close
+                    renderListP.remove(popMenu)
+                    print("remove check")
                 print('rightClickDetected')
                 menuX, menuY = event.pos
                 print(menuX, menuY)
                 popMenu = clickMenu(menuX, menuY, scale, 'A')
+                print("check:", popMenu)
                 popMenu.draw(screen)
+                renderListP.append(popMenu)
                 print('rightClickActionDone')
+            else:
+                if popMenu != 0:
+                    popMenu.close
 
     pygame.display.update()
 
